5323cf3719b7ec8903c722f7ab63a9360258c1ce
Ibad-Asif <ibadasif033@gmail.com>
2024-06-16 09:31:49 +0500

    Object of JS

---
+++ app.js
@@ -0,0 +1,61 @@
+// var student = {
+//     name : "Ibad",
+//     subject : "Coding",
+//     batch : "11",
+//     marks : "88",
+//     calculatorFees : () => {
+//         console.log("Hello, Guys");
+//     }
+// }
+// console.log("Student name :", student.name);
+// console.log("Student name :", student["name"]); //Dynamicali access krne k liye
+
+// student.calculatorFees()
+
+// let country = "country";
+// let value  = "Pakistan";
+
+// student[country] = value;
+// console.log(student);
+
+
+
+
+
+// my() => {
+//     console.log("hello, Guy")
+// }
+// my();
+
+
+// firstclass citizen in javascript.
+
+// var,boolean,number,array,object,function ye sare (first class) h.
+// kisi chiz m ye 3 bataen hen toh wo (first class) h ?
+// 1) kisi var m store ho sake
+// 2) kisi function m pass ho sake
+// 3) kisi function se return ho sake
+
+
+// CONSTRUCTURE FUNCTION 
+
+// function Student(name , roll){
+//     this.name = name,
+//     this.roll = roll
+
+// }
+// console.log(Student);
+// var student1 = new Student("Ibad",9513);
+// var student2 = new Student("Umar",9514);
+
+// // ---X---
+
+// var student3 = {
+//     name: "Ibad",
+//     roll: 123,
+//     age: 21
+// }
+// console.log(student3.hasOwnProperty("age"));
+// console.log("name" in student3);
+
+// console.log(student3);
